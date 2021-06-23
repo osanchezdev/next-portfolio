f67558c58d4043a620a9d8c5bf6f582da8103be3
osanchez <ojsm45@gmail.com>
2021-06-22 22:41:55 -0300

    Wip sidebar styles

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,20 +1,12 @@
-import React from "react"
-import Particles from "react-particles-js"
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <>
-      {/* <AboutSection /> */}
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        {/* <Image /> */}
-      </div>
-      <Particles />
-      {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+      {/* TODO: create all sections here */}
+      <h1>Home</h1>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/shared/layout/Layout.styles.ts
+++ src/components/shared/layout/Layout.styles.ts
@@ -1,9 +1,10 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const LayoutWrapper = styled.div`
+  height: 100vh;
   display: grid;
   grid-template-columns: 253px 1fr;
-`
+`;
 export const MainWrapper = styled.main`
   padding: 0 10rem;
-`
+`;
